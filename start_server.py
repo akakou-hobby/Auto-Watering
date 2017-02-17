f46f2c712da842e0fd27137ecaa16fb0fe48e79c
akakou <akakou571@gmail.com>
2017-02-17 11:23:16 +0900

    [update] マルチスレッドに変更

--- start_server.py
+++ start_server.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
 
 import CGIHTTPServer
-CGIHTTPServer.test()
+
+###サーバの起動
+def server():
+    CGIHTTPServer.test()
+
+###スレッドの立ち上げ
+def open_server():
+    # スレッドの設定
+    th = threading.Thread(target=open_server, name="server", args=(,))
+    th.start()
+    th.setDaemon(True)
