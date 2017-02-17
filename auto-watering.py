736fac8db5079600b76fb756c09fb305b4c8d2b5
akakou <akakou571@gmail.com>
2017-02-17 11:20:11 +0900

    [add] メインのcontralを追加

---
+++ auto-watering.py
@@ -0,0 +1,51 @@
+import commands
+import time
+import threading
+import start_server
+import hard_contral
+
+
+###セッティングデータの読み込み -----------------------------------------------------
+def get_setting_data():
+    ###ファイルの読み込み
+    def load_file():
+        file_data = ""
+        #ファイルの読み込み
+        file_name = open_file("CoH.txt")
+        open_files = open(file_name, 'r')
+        for row in open_files:
+            file_data = file_data + row
+        f.close()
+
+        return file_data
+
+    ###データの数値部分を取り出す
+    def set_int(string_num):
+        #テキストのデータを切り取って、整数型へ変換
+        result = string_num[7:]
+        result = int(string_num)
+        return result
+
+    set_data = load_file()
+    set_data = set_int(set_data)
+    return set_data
+
+
+###メイン -----------------------------------------------------------------------
+def main_contral():
+    #セッティングの受け取り
+    set_data = get_setting_data()
+    #センサーデータの受け取り
+    senser_data = commands.getoutput("sudo node senser.js")
+
+    #セッティングデータとセンサデータを比較し、センサーデータの方が小さかったら水をやる
+    if set_data > senser_data:
+        hard_contral.pump()
+
+
+if __name__ == '__main__':
+    start_server.open_server()
+
+    while True:
+        main_contral()
+        time.sleep(300)
