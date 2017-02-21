37d9fdb742135ccea30c64b8e682cb4f2485eea0
akakou <akakou571@gmail.com>
2017-02-21 11:47:22 +0900

    [fix] 様々な箇所（文字コード、onclickのoが抜けていたミス 、スレッド作成時の呼び出す関数の間違え等）の修正

--- auto-watering.py
+++ auto-watering.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import commands
 import time
-import threading
 import start_server
 import hard_contral
 
--- start_server.py
+++ start_server.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import CGIHTTPServer
+import threading
 
 ###サーバの起動
 def server():
@@ -9,6 +11,6 @@ def server():
 ###スレッドの立ち上げ
 def open_server():
     # スレッドの設定
-    th = threading.Thread(target=open_server, name="server", args=(,))
+    th = threading.Thread(target=server, name="server", args=())
     th.start()
     th.setDaemon(True)
