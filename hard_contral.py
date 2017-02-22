a51b244cebe917dd325807de57379fdd71284430
akakou <akakou571@gmail.com>
2017-02-22 15:45:44 +0900

    [fix] ピンを表す変数名をPUMPからPINに変更

--- hard_contral.py
+++ hard_contral.py
@@ -4,9 +4,9 @@ import RPi.GPIO as gpio
 import time
 
 ### 水やり
-def pump(PUMP,time_len):
-    gpio.setup(PUMP, gpio.OUT)
-    gpio.output(PUMP, 1)
+def pump(PIN,time_len):
+    gpio.setup(PIN, gpio.OUT)
+    gpio.output(PIN, 1)
     time.sleep(time_len)
-    gpio.output(PUMP, 0)
+    gpio.output(PIN, 0)
     gpio.cleanup()
